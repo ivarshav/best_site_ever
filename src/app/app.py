1275d04cba203b346ce7cae61c7c9adb5d6d5a14
Unknown <inbvar@gmail.com>
2020-10-24 22:13:44 +0300

    Add logic
    Add logic

---
+++ src/app/app.py
@@ -0,0 +1,43 @@
+import os
+
+import requests
+from flask import Flask, request
+from flask import render_template
+from requests import ConnectionError
+
+app = Flask(__name__)
+
+HOST = '0.0.0.0'
+URL = "http://www.omdbapi.com/?apikey=" + os.getenv('MY_API_KEY','')
+
+
+def get_results(title, year, show_type):
+    params = {
+        's': title,
+        'type': show_type,
+        'y': year
+    }
+    try:
+        response = requests.get(URL, params=params).json()
+    except ConnectionError:
+        return []
+    return response.get("Search", [])
+
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        year = request.form.get('year_input')
+        show_type = request.form.get('show_type')
+        res = get_results(title, year, show_type)
+        return render_template(
+            'index.html',
+            results=res
+        )
+    if request.method == 'GET':
+        return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(host=HOST, debug=True)
